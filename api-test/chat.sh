0f15bb245ff644f97e670cf723c9f8b78a4c879b
MathxH Chen <brainfvck@foxmail.com>
2023-05-30 10:20:03 +0800

    update chat secript

--- api-test/chat.sh
+++ api-test/chat.sh
@@ -37,3 +37,23 @@ curl -X 'POST' \
   "max_tokens": 1024,
   "stream": false
 }'
+
+curl -X 'POST' \
+  'https://be7c-222-172-244-12.ngrok-free.app/v1/chat/completions' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -H 'Authorization: Bearer ' \
+  -d '{
+  "messages": [
+    {
+      "role": "system",
+      "content": "You are a helpful assistant."
+    },
+    {
+      "role": "user",
+      "content": "What is the capital of France?"
+    }
+  ],
+  "max_tokens": 1024,
+  "stream": false
+}'
