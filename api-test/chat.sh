17016748a4cfa22bede3b0ecdc712130999a868d
MathxH Chen <brainfvck@foxmail.com>
2023-05-30 09:06:18 +0800

    Add chat script

---
+++ api-test/chat.sh
@@ -0,0 +1,19 @@
+curl -X 'POST' \
+  'https://be7c-222-172-244-12.ngrok-free.app/v1/chat/completions' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -H 'Authorization: Bearer sk-afwawfwadawfa' \
+  -d '{
+  "messages": [
+    {
+      "role": "system",
+      "content": "You are a helpful assistant."
+    },
+    {
+      "role": "user",
+      "content": "What is the capital of France?"
+    }
+  ],
+  "max_tokens": 1024,
+  "stream": false
+}'
